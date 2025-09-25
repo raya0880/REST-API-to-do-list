f1a12c26bc3aedb4609dc1d0817def60821752ae
raya0880 <168654909+raya0880@users.noreply.github.com>
2025-09-25 21:38:32 +0900

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,13 +6,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 db = SQLAlchemy()
-
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///todo.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+
+    with app.app_context():
+        from .models import Task 
+        db.create_all()
 
     from .routes import bp
     app.register_blueprint(bp)
