e11b08e6c6515c905b9f38159e9bd1997a9d3bae
raya0880 <168654909+raya0880@users.noreply.github.com>
2025-09-25 21:43:20 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,3 @@
-from app import create_app, db
+from app import create_app
 
 app = create_app()
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, host='0.0.0.0')
