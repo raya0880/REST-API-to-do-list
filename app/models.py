f14e5d5bc26fd71546e8f56f1506153c48ac0f90
raya0880 <168654909+raya0880@users.noreply.github.com>
2025-09-25 18:27:12 +0900

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from . import db
+from datetime import datetime
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    done = db.Column(db.Boolean, default=False, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'done': self.done,
+            'created_at': self.created_at.isoformat()
+        }
